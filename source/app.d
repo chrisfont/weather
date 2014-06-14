5c8c1edd41cdd0c742b2bc71da35608a45ca9440
Chris Font <cfont3@uic.edu>
2014-06-14 15:39:03 -0500

    Test Code
    - Added some "test" code to app.d
    - Updated .gitignore(initial) to ignore project files and binaries.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,15 @@
 import std.stdio;
 
+import conf;
+
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	config configuration = new config();
+
+	configuration.set_location("37.8267,-122.423");
+	configuration.set_api_key("");
+
+	writeln("Config: ", configuration.get_url_now());
+
+	getchar();
 }
